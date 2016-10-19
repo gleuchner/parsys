467a893c33324583f1795ba12279314c9c8df679
gerrit <gerrit.leuchner@gmail.com>
2016-10-19 22:38:13 +0200

    Terminate, clean up

--- src/parallel/systems/a1/TakeAway.java
+++ src/parallel/systems/a1/TakeAway.java
@@ -6,17 +6,19 @@ public class TakeAway {
 
 	private static final int INTERVAL = 5;
 
-	private static final int TIME_MIN = 4;
+	private static final int TIME_MIN = 1;
 
-	private static final int TIME_MAX = 7;
+	private static final int TIME_MAX = 4;
 
-	private static final int SIZE_MIN = 2;
+	private static final int SIZE_MIN = 1;
 
-	private static final int SIZE_MAX = 5;
+	private static final int SIZE_MAX = 10;
 
 	private static final int NUM_EMPLOYEES = 3;
 
 	private static final int NUM_GROUPS = 20;
+	
+	private int groupsLeft;
 
 	private int _orderedDoeners = 0;
 
@@ -25,34 +27,41 @@ public class TakeAway {
 	public static void main(String args[]) {
 		final TakeAway takeaway = new TakeAway();
 		for (int i = 0; i < NUM_EMPLOYEES; i++) {
-			new Thread(new Runnable() {
+			Thread t = new Thread(new Runnable() {
 
 				@Override
 				public void run() {
-					while (true) {
+					while (!takeaway.finished()) {
 						takeaway.serve();
 					}
 				}
-			}, "Employee-" + i).start();
+			}, "Employee-" + i);
+			t.start();
 		}
 
 		for (int i = 0; i < NUM_GROUPS; i++) {
-			new Thread(new Runnable() {
+			Thread t = new Thread(new Runnable() {
 
 				@Override
 				public void run() {
 					int size = ThreadLocalRandom.current().nextInt(SIZE_MIN, SIZE_MAX + 1);
+
 					System.out.println(Thread.currentThread().getName() + " entering with " + size + " pupils");
+
 					takeaway.order(size);
 					takeaway.waitForOrder(size);
 				}
-			}, "Group-" + i).start();
+			}, "Group-" + i);
+			t.start();
 			try {
 				Thread.sleep(INTERVAL * 1000);
 			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
 		}
+	}
+
+	public synchronized boolean finished() {
+		return groupsLeft == NUM_GROUPS;
 	}
 
 	public synchronized void order(int amount) {
@@ -69,31 +78,30 @@ public class TakeAway {
 			try {
 				this.wait();
 			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
 		}
 		_preparedDoeners -= amount;
+
 		System.out.println(Thread.currentThread().getName() + " leaving");
 		System.out.println("Ordered: " + _orderedDoeners + "\tPrepared: " + _preparedDoeners);
+		
+		groupsLeft++;
 	}
 
-	public void serve() {
-		while (true) {
-			synchronized (this) {
-				if (_orderedDoeners > 0) {
-					try {
-						int time = ThreadLocalRandom.current().nextInt(TIME_MIN, TIME_MAX + 1);
-						System.out.println(Thread.currentThread().getName() + " preparing doener in " + time + "s");
-						_orderedDoeners--;
-						_preparedDoeners++;
-						System.out.println("Ordered: " + _orderedDoeners + "\tPrepared: " + _preparedDoeners);
-						Thread.sleep(time * 1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					this.notify();
-				}
+	public synchronized void serve() {
+		if (_orderedDoeners > 0) {
+			int time = ThreadLocalRandom.current().nextInt(TIME_MIN, TIME_MAX + 1);
+
+			_orderedDoeners--;
+			_preparedDoeners++;
+
+			System.out.println(Thread.currentThread().getName() + " preparing doener in " + time + "s");
+			System.out.println("Ordered: " + _orderedDoeners + "\tPrepared: " + _preparedDoeners);
+			try {
+				Thread.sleep(time * 1000);
+			} catch (InterruptedException e) {
 			}
+			this.notify();
 		}
 	}
 }
